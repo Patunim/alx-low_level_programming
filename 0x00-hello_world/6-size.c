c0c8debdd38d87334c766524f4b7e13606d012c1
patunim <patrickunim@gmail.com>
2022-10-13 21:35:35 -0700

    seventh commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an init: %i byte(s)\n", sizeof(init));
+	printf("Size of a long init: %i byte(s)\n", sizeof(long init));
+	printf("Size of a long long init: %i byte(s)\n", sizeof(long long init));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
